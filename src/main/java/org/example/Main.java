e355fcaaeef514f6507e465d82102f3288cb3d4b
Tobias Weber <toby-weber@gmx.de>
2024-10-09 11:19:37 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        /*
+        * Create a Java record "Animal" that should have properties for ID, Name, Species, and Age. Choose suitable field names.
+        * */
+    }
+}
